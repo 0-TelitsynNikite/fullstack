7c44ec1fc836b112bdb0ea9b08ab908c26f091e9
Телицын Никита <dmtvn.labs@yandex.ru>
2022-07-24 16:50:22 +0300

    Added JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,71 @@
+// import libraries
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+// import another files
 const db = require('../db')
+
 
 class LoginOrRegisterUser {
 
-    //sign in person
+    // Sign in person
     async login(req, res) {
-        const {name, surname} = req.body
-        const newPerson = await db.query(`INSERT INTO posts (name, surname) values ($1, $2) RETURNING *`, [name, surname])
-        res.json(newPerson)
+        const {email, password} = req.body
+        const candidate = await db.query(`SELECT * FROM users WHERE email = $1`, [email])
+
+        if (candidate.rowCount) {
+            const passwordResult = bcrypt.compareSync(password, candidate.rows[0].password)
+
+            if (passwordResult) {
+                // Let's generate TOKEN
+
+                const token = jwt.sign({
+                    email: email,
+                    userId: candidate.rows[0].id
+                }, process.env.DEV_JWT, {expiresIn: 3600})
+
+                res.status(200).json({
+                    token: `Bearer ${token}`
+                })
+            } else {
+                // Passwords did not match
+
+                res.status(401).json({
+                    message: 'Неверно указан пароль'
+                })
+            }
+        } else {
+            res.status(404).json({
+                message: 'Пользователь с таким email не найден'
+            })
+        }
     }
 
-    //sign up person
+    // Sign up person
     async register(req, res) {
-        const {name, surname} = req.body
-        const newPerson = await db.query(`INSERT INTO posts (name, surname) values ($1, $2) RETURNING *`, [name, surname])
-        res.json(newPerson)
+        const {email, password} = req.body
+        const candidate = await db.query(`SELECT * FROM users WHERE email = $1`, [email])
+
+        if (candidate.rowCount) {
+            // There's the user in the table
+
+            res.status(409).json({
+                message: 'Такой email уже занят'
+            })
+        } else {
+            // Let's make a user
+
+            const salt = bcrypt.genSaltSync(10)
+            const userPassword = password
+            const user = await db.query(`INSERT INTO users (email, password) values ($1, $2) RETURNING *`, [email, bcrypt.hashSync(password, salt)])
+
+            try {
+                res.status(201).json(user.rows)
+            } catch(e) {
+                console.error(e)
+            }
+
+        }
     }
 }
 
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express')
 const controller = require('../controllers/auth')
 const router = express.Router()
 
+// localhost:4200/api/login
 router.post('/login', controller.login)
+
+// localhost:4200/api/register
 router.post('/register', controller.register)
 
 module.exports = router
